755a80ee020580b97a13498ab4637c9d23388b20
Sanjay D S <dssanjay764@gmail.com>
2026-07-12 13:52:41 +0530

    12-07-2026 - Swap Two Numbers without temp

---
+++ Logic_Building_Problems/Basic/Swap Two Numbers without temp.py
@@ -0,0 +1,9 @@
+#Swap Two Numbers without temp | Date:12-07-2026
+
+num1=int(input("Enter the value of num1: "))
+num2=int(input("Enter the value of num2: "))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("The value of num1 is",num1)
+print("the value of num1 is",num2)
